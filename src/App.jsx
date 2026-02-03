793dc8ee1600bfeb4dc0fc17678d23e5d5ad2c19
Fatima-Ezzahra33 <Fatima-Ezzahra33@users.noreply.github.com>
2026-02-03 09:46:45 +0100

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,29 @@
+import React from 'react'
+import NavBar from '@/layout/NavBar.jsx'
+import Hero from '@/sections/Hero.jsx'
+import About from '@/sections/About.jsx'
+import Skills from '@/sections/Skills.jsx'
+import Projects from '@/sections/Projects.jsx'
+import Contact from '@/sections/Contact.jsx'
+import Experience from '@/sections/Experience'
+
+
 
 
 function App() {
 
   return (
-    <>
-      <h1>Hiii</h1>
-    </>
+    <div className="min-h-screen overflow-x-hidden">
+      <NavBar/>
+      <main>
+        <Hero/>
+        <About/>
+        <Skills/>
+        <Experience/>
+        <Projects/>
+        <Contact/>
+      </main>
+    </div>
   )
 }
 
